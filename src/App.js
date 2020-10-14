4c73473ec357ed89f6468df69154088a90e9319c
Archana Shokeen <archanashokeen@Archanas-MacBook-Air.local>
2020-10-14 15:56:59 -0700

    auth login and signup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Header } from "semantic-ui-react";
+// import "./App.css";
+import Amplify from "aws-amplify";
+
+import aws_exports from "./aws-exports";
+import { withAuthenticator, AmplifySignOut } from "@aws-amplify/ui-react";
+// import { withAuthenticator } from "aws-amplify-react";
+Amplify.configure(aws_exports);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button>
+        <AmplifySignOut />
+      </button>
+      <Header as="h1">Hello World!</Header>
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App, { includeGreetings: true });
